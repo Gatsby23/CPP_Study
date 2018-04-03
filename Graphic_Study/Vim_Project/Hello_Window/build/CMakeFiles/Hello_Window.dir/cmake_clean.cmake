76ed13d03a3a808b5800ac55a4d27acd8945d989
Gatsby23 <pet_wolf23@163.com>
2018-04-03 10:51:03 +0800

    在Linux下搭建好了OpenGL项目

---
+++ Graphic_Study/Vim_Project/Hello_Window/build/CMakeFiles/Hello_Window.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_Window.dir/Hello_Window.cpp.o"
+  "Hello_Window.pdb"
+  "Hello_Window"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_Window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
