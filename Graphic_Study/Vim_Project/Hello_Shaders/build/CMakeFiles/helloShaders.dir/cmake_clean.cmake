ba8ffcb40de22d658102886dc7cebbc170dc8df3
Gatsby23 <pet_wolf23@163.com>
2018-06-27 09:42:40 +0800

    The study of Shader Compiler

---
+++ Graphic_Study/Vim_Project/Hello_Shaders/build/CMakeFiles/helloShaders.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloShaders.dir/src/hello_Shaders.cpp.o"
+  "CMakeFiles/helloShaders.dir/src/glad.c.o"
+  "helloShaders.pdb"
+  "helloShaders"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/helloShaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
